db6eb87267cd00031745bb6f2baeb802010ac62a
gayathri2016 <gayathri_m@live.com>
2016-09-28 10:24:07 -0700

    added editing list functionality and persisting data to a file

--- app/src/main/java/com/example/gayathrim/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/gayathrim/simpletodo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gayathrim.simpletodo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,21 +19,29 @@ import static com.example.gayathrim.simpletodo.R.id.lvItems;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String>todoItems;
+    ArrayList<String> todoItems;
     ArrayAdapter<String> aToDOAdapter;
     ListView Items;
     EditText etEditText;
+    private final int REQUEST_CODE = 20;
+    String name;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         populateArrayItems();
-        Items = (ListView)findViewById(lvItems);
+        Items = (ListView) findViewById(lvItems);
         Items.setAdapter(aToDOAdapter);
-        etEditText = (EditText)findViewById(R.id.etEditText);
+        etEditText = (EditText) findViewById(R.id.etEditText);
+
+        onActivityResult(REQUEST_CODE, RESULT_OK, getIntent());
+
         Items.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id){
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 todoItems.remove(position);
                 aToDOAdapter.notifyDataSetChanged();
                 writeItems();
@@ -42,27 +51,55 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        Items.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                Intent editScreen = new Intent(getApplicationContext(), editActivity.class);
+                String selectedFromList = (String) (Items.getItemAtPosition(position));
+                Bundle extras = new Bundle();
+                extras.putSerializable("position", position);
+                extras.putSerializable("obj_to_pass", selectedFromList);
+                editScreen.putExtras(extras);
+                todoItems.remove(position);
+                aToDOAdapter.notifyDataSetChanged();
+                writeItems();
+                startActivity(editScreen);
+            }
+
+
+        });
+
     }
-    public  void populateArrayItems(){
-       readItems();
+    public void onClick(View view) {
+        Intent i = new Intent(MainActivity.this, editActivity.class);
+        i.putExtra("mode", 2); // pass arbitrary data to launched activity
+        startActivityForResult(i, REQUEST_CODE);
+    }
+
+    public void populateArrayItems() {
+        readItems();
         aToDOAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
     }
+
     private void readItems() {
         File filesDir = getFilesDir();
-        File file = new File(filesDir, "todo.txt");
+        File file = new File(filesDir, "todo1.txt");
         try {
             todoItems = new ArrayList<String>(FileUtils.readLines(file));
-        }catch(IOException e){
+        } catch (IOException e) {
             todoItems = new ArrayList<String>();
         }
 
     }
+
     private void writeItems() {
         File filesDir = getFilesDir();
-        File file = new File(filesDir, "todo.txt");
+        File file = new File(filesDir, "todo1.txt");
         try {
-           FileUtils.writeLines(file, todoItems);
-        }catch(IOException e){
+            FileUtils.writeLines(file, todoItems);
+        } catch (IOException e) {
+            todoItems = new ArrayList<String>();
 
         }
 
@@ -74,4 +111,27 @@ public class MainActivity extends AppCompatActivity {
         etEditText.setText("");
         writeItems();
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // REQUEST_CODE is defined above
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE && data.getExtras() != null) {
+            // Extract name value from result extras
+            name = data.getExtras().getString("name");
+            int code =  data.getExtras().getInt("position");
+            if(!name.isEmpty()) {
+                todoItems.add(code, name);
+                aToDOAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+
+
+
+
+
+
+
+
 }
